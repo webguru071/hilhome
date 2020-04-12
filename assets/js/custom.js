d955dfde943cbe3e82916047d12324cb9f7ae459
Suraiya-Aysha <sm.asa15nov@gmail.com>
2020-04-12 20:20:55 +0600

    doing guarantee page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -183,6 +183,39 @@ $('.venobox').venobox();
     });
 // Home Recent Questions Slider
 
+// Guarantee Fixed Nav Tabs
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $('.guarantee-menu').addClass("sticky-menu");
+        } else {
+            $('.guarantee-menu').removeClass("sticky-menu");
+        }
+    });
+// Guarantee Fixed Nav Tabs
+
+//Accordion Box
+    if($('.accordion-box').length){
+        $(".accordion-box").on('click', '.acc-btn', function() {
+
+            var outerBox = $(this).parents('.accordion-box');
+            var target = $(this).parents('.accordion');
+
+            if($(this).hasClass('active')!==true){
+                $(outerBox).find('.accordion .acc-btn').removeClass('active');
+            }
+
+            if ($(this).next('.acc-content').is(':visible')){
+                return false;
+            }else{
+                $(this).addClass('active');
+                $(outerBox).children('.accordion').removeClass('active-block');
+                $(outerBox).find('.accordion').children('.acc-content').slideUp(300);
+                target.addClass('active-block');
+                $(this).next('.acc-content').slideDown(300);
+            }
+        });
+    }
+
   //--------- Initialize WOW JS
   new WOW().init();
 
